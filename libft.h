ca44710ee49fd5f1b9245f8c895de8f485664298
mathispeyre <mathispeyre0@gmail.com>
2024-11-04 21:59:16 +0100

    innit & kickoff day 42

---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 13:13:58 by mathispeyre       #+#    #+#             */
+/*   Updated: 2024/11/04 16:08:56 by mathispeyre      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+int	ft_atoi(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+#endif
---
+++ makefile
@@ -0,0 +1,27 @@
+NAME = libft.a
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+FILES = ft_strlen.c ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c \
+        ft_isprint.c ft_toupper.c ft_tolower.c ft_atoi.c ft_strlcpy.c \
+        ft_strlcat.c
+
+HEAD = libft.h
+OBJ = $(FILES:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	ar rcs $(NAME) $(OBJ)
+
+%.o: %.c $(HEAD)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
