6c167375f7bb5075c57f14550c299a07f6f46db8
mpeyre-s <mathispeyre0@gmail.com>
2024-11-07 17:03:11 +0100

    ft_putendl_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:13:58 by mathispeyre       #+#    #+#             */
-/*   Updated: 2024/11/07 16:53:20 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2024/11/07 17:00:16 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
