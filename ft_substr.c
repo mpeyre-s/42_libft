6d5730f7875595cc4945c70df65ad9df07d289f7
mpeyre-s <mathispeyre0@gmail.com>
2024-11-08 09:38:58 +0100

    fix substr if when len > strlen(s)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:27:37 by mathispeyre       #+#    #+#             */
-/*   Updated: 2024/11/06 15:22:58 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2024/11/08 09:37:09 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
+	size_t	size;
 	char	*result;
 
 	i = start;
 	j = 0;
-	result = (char *)malloc(len + 1);
+	size = len;
+	if (len > ft_strlen(s))
+		size = ft_strlen(s);
+	result = (char *)malloc(size + 1);
 	if (!result || !s)
 		return (0);
 	while (i < ft_strlen(s) && j < len)
