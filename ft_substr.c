20cfe370b666d96c16ded519820a1fb83893bd9d
mpeyre-s <mathispeyre0@gmail.com>
2024-11-14 10:20:33 +0100

    substr (very last version)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:27:37 by mathispeyre       #+#    #+#             */
-/*   Updated: 2024/11/14 10:02:12 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2024/11/14 10:20:10 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*result;
 
-	i = start;
-	j = 0;
 	if (!s)
 		return (0);
 	if (start >= ft_strlen(s))
 		len = 0;
-	if (len > (ft_strlen(s + start)))
+	else if (len > ft_strlen(s + start))
 		len = ft_strlen(s + start);
 	result = (char *)malloc(len + 1);
 	if (!result)
 		return (0);
-	while (i < ft_strlen(s))
+	i = start;
+	j = 0;
+	while (j < len && s[i])
 		result[j++] = s[i++];
 	result[j] = 0;
 	return (result);
 }
+
